fe091dba91aade87133ad0f34422fc9495dc17c0
lcapra <luca.capra@spindox.it>
2024-03-22 16:54:12 +0100

    add xr topic

--- src/constants.ts
+++ src/constants.ts
@@ -5,10 +5,9 @@ import {
 
 export const DEFAULT_AVATAR_LANGUAGE = 'en-GB';
 
+export const XRMARKER_DETECTION = 'app/:appId/xr/marker/detected';
 export const USER_CHARACTERIZATION_TOPIC = 'detection/characterization';
-
 export const AUDIO_CLASSIFICATION_TOPIC = 'detection/audio';
-
 export const UI_INTERACTION_TOPIC = 'ui/interaction';
 
 export const DefaultModel: RepositoryAvatarModelDto = {
