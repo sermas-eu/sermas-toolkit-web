a8855ee17054a47c9bc23883163888bca388a0f9
lc-spxl <luca.capra@spindox.it>
2024-05-09 17:45:55 +0200

    use chunkId as number

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,4 @@ export { FpsMonitor, type PerformanceEvent } from './fps.js';
 export { addGlobal } from './global.js';
 export { Logger, logger } from './logger.js';
 
-export const getChunkId = () => Date.now() + performance.now();
+export const getChunkId = () => +(Date.now() + performance.now());
