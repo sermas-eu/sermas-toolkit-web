93f43fdb887ae79dae73aef6660076a9c17e9a83
lc-spxl <luca.capra@spindox.it>
2024-05-10 05:47:41 +0200

    add log filter

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,27 @@
+import { addGlobal } from './global.js';
+
 type FormatterCallback = (v?: any[]) => any[];
 
-export type LogLevel = 'DEBUG' | 'LOG' | 'WARN' | 'ERROR';
+export type LogLevel = 'VERBOSE' | 'DEBUG' | 'LOG' | 'WARN' | 'ERROR';
 
 export const DefaultLogLevel: LogLevel = 'DEBUG';
 
 const levels: Record<LogLevel, number> = {
+  VERBOSE: 50,
   DEBUG: 100,
   LOG: 200,
   WARN: 300,
   ERROR: 400,
+};
+
+let logFilter: string | undefined = '*';
+
+// set log filter
+// empty / undefined = none
+// * = all
+// prefix = filter by logger prefix
+export const setLogFilter = (filter?: string) => {
+  logFilter = filter;
 };
 
 export class Logger {
@@ -27,26 +40,36 @@ export class Logger {
     return [`${this.showDates ? new Date() + ' ' : ''}[${this.prefix}]`, ...v];
   }
 
+  isLogFiltered(level: number) {
+    // console.warn(this.prefix, levels[this.logLevel], level);
+    if (levels[this.logLevel] < level) return false;
+    if (logFilter === undefined) return true;
+    if (logFilter === '*') return false;
+    return this.prefix.toLowerCase().indexOf(logFilter.toLowerCase()) === -1;
+  }
+
   clear() {
     console.clear();
   }
 
   debug(...v: any[]) {
-    if (levels[this.logLevel] !== levels.DEBUG) return;
+    if (this.isLogFiltered(levels.VERBOSE)) return;
     console.debug(...this.format('DEBUG', v));
   }
   log(...v: any[]) {
-    if (levels[this.logLevel] > levels.DEBUG) return;
+    if (this.isLogFiltered(levels.DEBUG)) return;
     console.log(...this.format('LOG', v));
   }
   warn(...v: any[]) {
-    if (levels[this.logLevel] > levels.LOG) return;
+    if (this.isLogFiltered(levels.LOG)) return;
     console.warn(...this.format('WARN', v));
   }
   error(...v: any[]) {
-    if (levels[this.logLevel] > levels.WARN) return;
+    if (this.isLogFiltered(levels.WARN)) return;
     console.error(...this.format('ERROR', v));
   }
 }
 
 export const logger = new Logger('default');
+
+addGlobal('setLogFilter', setLogFilter);
