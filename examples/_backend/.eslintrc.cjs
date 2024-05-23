77066bfb98e69fc998435ac19577ba8354cf2593
Davide Farina <davide.farina@spindox.it>
2024-05-23 12:31:09 +0200

    initialize simple backend to retrieve access tokens

---
+++ examples/_backend/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
---
+++ examples/_backend/src/index.ts
@@ -0,0 +1,42 @@
+import express, { Express, Request, Response } from "express";
+import {
+  getAvatarToken,
+  getAvatarTokenWithClientSecret,
+} from "@sermas/sermas-toolkit-node-sdk";
+
+const app: Express = express();
+const port = 3000;
+
+const PRIVATE_API_BASE_URL =
+  process.env.PRIVATE_API_BASE_URL || "http://172.17.0.1:8080/api";
+
+app.use(express.json());
+app.use("*", (req, res, next) => {
+  console.log(`[server]: Request arrived - ${req.method} ${req.path}`);
+  next();
+  console.log(`[server]: Sending response - ${res.statusCode} ${res.json}`);
+});
+
+app.get("/auth/public/:appId", async (req: Request, res: Response) => {
+  const appId = req.params.appId;
+
+  return res.json(
+    await getAvatarToken(appId, {
+      PRIVATE_API_BASE_URL,
+    }),
+  );
+});
+
+app.get("/auth/private/:appId", async (req: Request, res: Response) => {
+  const appId = req.params.appId;
+
+  return res.json(
+    await getAvatarTokenWithClientSecret(appId, {
+      PRIVATE_API_BASE_URL,
+    }),
+  );
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
